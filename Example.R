7a3db0d6313630f5d292bd56ff28c7d3420c4849
uhkniazi <uhkniazi@users.noreply.github.com>
2015-12-03 10:55:54 +0000

    testing

--- Example.R
+++ Example.R
@@ -13,3 +13,18 @@ csPath = file.choose()
 ob = CFastqQuality(csPath, sample.name = 'SRR850132_2.fastq')
 
 CFastqQuality.plot.cycle(ob)
+
+
+
+fls <- dir(path = '../temp/RNASeq/20151110/FASTQ/', pattern = "*fastq.gz", full=TRUE)
+
+coll <- QACollate(QAFastqSource(fls), QAReadQuality(),
+                  QAAdapterContamination(), QANucleotideUse(),
+                  QAQualityUse(), QASequenceUse(),
+                  QAFrequentSequence(n=10), QANucleotideByCycle(),
+                  QAQualityByCycle())
+x <- qa2(coll,  verbose=TRUE)
+
+res <- report(x, dest = 'Temp')
+if (interactive())
+  browseURL(res)
