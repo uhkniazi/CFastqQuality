d4d13ce9ba8a788269a96aab1a123b94fde1c130
Umar Niazi <uhkniazi@gmail.com>
2019-08-13 11:03:04 +0100

    testing after adding new samples and meta data

--- Example.R
+++ Example.R
@@ -36,20 +36,34 @@ if (interactive())
 ############## example of using multiple fastq files 
 source('CFastqQuality.R')
 
-## create a data set 
-## the original fastq files are not included in this example due to size
-## use your own fastq files 
-paths = dir('Data_external/', pattern='*.fastq.gz', full=T)
+###### load data and metadata
+## some sample files are included in the sampleData directory
+dfMetaData = read.csv('sampleData/metaData.csv', header=T)
+dfMetaData$read_direction = factor(dfMetaData$read_direction)
+str(dfMetaData)
 
-# > paths
-# [1] "Data_external//Plate1-C02_S4_L008_R1_001.fastq.gz"  "Data_external//Plate1-C02_S4_L008_R2_001.fastq.gz" 
-# [3] "Data_external//Plate1-C06_S19_L008_R1_001.fastq.gz" "Data_external//Plate1-C06_S19_L008_R2_001.fastq.gz"
+## load fastq file names
+paths = dir('sampleData/', pattern='*.fastq.gz', full=F)
 
-fReadDirection = factor(c(1, 2, 1, 2))
-cNames = c('P1-C2-R1', 'P1-C2-R2', 'P1-C6-R1', 'P1-C6-R2')
-lMetaData = list(grouping=factor(c('Treatment', 'Treatment', 'Control', 'Control')))
+## check if file names are identical to the names from meta data sheet
+table(paths %in% as.character(dfMetaData$file_name))
+
+## set working directory to directory with fastq files
+cCurDir = getwd()
+setwd('sampleData/')
+
+paths = as.character(dfMetaData$file_name)
+
+# set factors required for analysis
+fReadDirection = factor(dfMetaData$read_direction)
+cNames = paste0(as.character(dfMetaData$sample_name), '_', as.character(dfMetaData$read_direction), as.character(dfMetaData$time_point))
+# any additional factors that need including in the metadata list
+lMetaData = list(meta=dfMetaData)
+
+## create object, go have a cup of coffee while this runs
 ob = CFastqQualityBatch(paths, cNames, fReadDirection, lMetaData)
-
+setwd(cCurDir)
+#### some functions to make plots
 ## get read counts in millions
 iGetReadCount(ob)
 barplot.readcount(ob)
@@ -67,28 +81,38 @@ source('CDiagnosticPlots.R')
 # delete the file after source
 unlink('CDiagnosticPlots.R')
 
-fReadDirection = ob@fReadDirection
+## extract the base quality matrix 
 mBatch = mGetReadQualityByCycle(ob)
 dim(mBatch)
 mBatch[1:10, 1:4]
 
+## creat an object of diagnostics class to make plots
 oDiag = CDiagnosticPlots(mBatch, 'Base Quality')
+
+## turning off automatic jitters
 ## we set jitter to FALSE for PCA, otherwise, in sparse matrix a random jitter is added to avoid divisions by zeros
 l = CDiagnosticPlotsGetParameters(oDiag)
 l$PCA.jitter = F; l$HC.jitter = F;
 oDiag = CDiagnosticPlotsSetParameters(oDiag, l)
 
-plot.mean.summary(oDiag, fReadDirection)
-plot.sigma.summary(oDiag, fReadDirection)
-boxplot.median.summary(oDiag, fReadDirection)
-plot.PCA(oDiag, fReadDirection)
-plot.dendogram(oDiag, fReadDirection, labels_cex = 0.8)
+## try some various factors to make the plots of low dimensional summaries
+plot.mean.summary(oDiag, ob@lMeta$meta$read_direction)
+plot.sigma.summary(oDiag, ob@lMeta$meta$read_direction)
+boxplot.median.summary(oDiag, ob@lMeta$meta$read_direction)
+plot.PCA(oDiag, ob@lMeta$meta$read_direction)
+plot.dendogram(oDiag, ob@lMeta$meta$read_direction, labels_cex = 0.8)
 
+## it appears files from K1_1 time point 2 and K1_2 time point 2 are outliers
+## plot the quality for these 
+plot.qualitycycle(ob@lData$K1_1t2)
+plot.alphabetcycle(ob@lData$K1_1t2)
+## compare this with a time point 1 sample
+plot.alphabetcycle(ob@lData$K1_1t1)
+
+## change covariate for plotting 
 ## the covariate for plotting can be changed to something new of choice
-lMetaData = CFastqQualityBatch.getMetaData(ob)
-levels(lMetaData$grouping)
-plot.mean.summary(oDiag, lMetaData$grouping)
-plot.sigma.summary(oDiag, lMetaData$grouping)
-boxplot.median.summary(oDiag, lMetaData$grouping)
-plot.PCA(oDiag, lMetaData$grouping)
-plot.dendogram(oDiag, lMetaData$grouping, labels_cex = 0.8)
+plot.mean.summary(oDiag, ob@lMeta$meta$time_point)
+plot.sigma.summary(oDiag, ob@lMeta$meta$time_point)
+boxplot.median.summary(oDiag, ob@lMeta$meta$time_point)
+plot.PCA(oDiag, ob@lMeta$meta$time_point)
+plot.dendogram(oDiag, ob@lMeta$meta$time_point, labels_cex = 0.8)
